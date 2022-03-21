a33c2310dc56534617a4dbd173bac922ac9f6186
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-03-21 11:50:34 +0700

    menambahkan content untuk SinglePost component

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+import Moment from "react-moment";
+import Loader from "../skull.webp";
 
 const SinglePost = (props) => {
     const [loading, setLoading] = useState(false);
@@ -29,6 +32,30 @@ const SinglePost = (props) => {
     return(
         <div>
             <Navbar/>
+            {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+            {Object.keys(post).length ? (
+                <div className="mt-5 post-container">
+                    <div key={post.id} className="card border-dark mb-3" style={{width :'50rem'}}>
+                        {/* TITLE */}
+                        <div className="card-header">
+                            {post.title.rendered}
+                        </div>
+
+                        {/* BODY */}
+                        <div className="card-body">
+                            <div className="card-text post-content">
+                                {ReactHtmlParser(post.content.rendered)}                
+                            </div>
+                        </div>
+
+                        {/* FOOTER */}
+                        <div className="card-footer">
+                            <Moment fromNow>{post.date}</Moment>
+                        </div>
+                    </div>
+                </div>
+            ) : ("")}
+            {loading && <img className="loader" src={Loader} alt="Loader"/>}
         </div>
     )
 }
